fd996e51e2e313072fb559a659c47a29ad8fa500
ChunChunH <agussolheredia@gmail.com>
2021-06-10 11:34:17 -0300

    msg action not allowed

--- middlewares/requiredRole.js
+++ middlewares/requiredRole.js
@@ -5,7 +5,10 @@ const requiredRole = (requiredRole) => {
       if(req.userType === requiredRole) {
         return next();
       } else {
-        return res.status(401).send('Action not allowed');
+        return res.status(401).json({
+          ok: false,
+          msg: "Action not allowed"
+        });
       }
     }
   }
