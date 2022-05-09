978367b5015b46f159479ed3d910e69a094430a7
Agustina Heredia <69329604+ChunChunH@users.noreply.github.com>
2022-05-09 10:38:19 -0300

    expiresIn 20 days

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -7,7 +7,9 @@ const generateJWT = (id, name, userType) => {
         console.log("NAME IN GENERATEJWT", name)
         const payload = {id, name, userType}
 
-        jwt.sign(payload, process.env.SECRET_JWT_SEED, (err, token) => {
+        jwt.sign(payload, process.env.SECRET_JWT_SEED, {
+            expiresIn: "20d",
+        }, (err, token) => {
           
             if (err){
                 console.log(err)
